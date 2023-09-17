be03b28d41d4c558cd84a88a200c4d1602095390
PashMato <98494791+pashmato@users.noreply.github.com>
2023-09-17 22:25:57 +0300

    Download ShapeGL

---
+++ Core/GameObject.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using Endgine3d.Body;
+
+namespace _3dEngine.Core
+{
+    
+    public class GameObject
+    {
+        // All of the game objects
+        protected static List<GameObject> AllGameObjects {get; private set; } = new List<GameObject>();
+        
+        // non-static fields
+        public float[] Pos {get; private set;} = new float[3]{0f, 0f, 0f};
+        public float[] Rot {get; private set;} = new float[3]{0f, 0f, 0f};
+
+        public string name { get; private set; } = "GameObject";
+
+        public Mesh mesh { get; private set; } = new Mesh();
+
+        public GameObject(string _name)
+        {
+            AllGameObjects.Add(this);
+            
+            name = _name;
+        }
+
+        public GameObject()
+        {
+            AllGameObjects.Add(this);
+            mesh.CheckVaild();
+        }
+
+        public GameObject(string _name, float[] pos, float[] rot)
+        {
+            AllGameObjects.Add(this);
+            mesh.CheckVaild();
+
+            name = _name;
+            
+            Pos = pos;
+            Rot = rot;
+        }
+        
+        public GameObject(string _name, float[] pos, float[] rot, Mesh m)
+        {
+            AllGameObjects.Add(this);
+            mesh.CheckVaild();
+
+            name = _name;
+            
+            mesh = m;
+
+            Pos = pos;
+            Rot = rot;
+        }
+
+        ~GameObject()
+        {
+            AllGameObjects.Remove(this);
+            mesh.CheckVaild();
+        }
+    }
+}
